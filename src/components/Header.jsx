4b3c716082821be593915caeadec8611fe4545ed
jcruzbravo <L18390012@chetumal.tecnm.mx>
2023-03-03 16:44:27 -0500

    Add Header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { NavLink, Link } from "react-router-dom";
+import logo from "../assets/logo.png";
+import '../styles/Header.scss';
+
+const Header = () => {
+  return (
+    <header>
+      <Link to="/">
+        <img src={logo} alt="home"/>
+      </Link>
+      <nav>
+        <ul>
+          <li>
+            <NavLink to="/shop">Shop</NavLink>
+          </li>
+          <li>
+            <NavLink to="/map">Map</NavLink>
+          </li>
+          <li>
+            <NavLink to="/challenges">Challenges</NavLink>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
