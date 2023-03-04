9b19fe54f2e031a7c0efdfc8e21a82f0a0d14106
jcruzbravo <L18390012@chetumal.tecnm.mx>
2023-03-04 13:28:38 -0500

    Add redirect links to social networks

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,13 +6,13 @@ import {
   FaGithub,
   FaInstagram,
 } from "react-icons/fa";
-import '../styles/Footer.scss';
+import "../styles/Footer.scss";
 
-const colorFacebook =  '#3b5998';
-const colorTwitter = '#55acee';
-const colorInstagram = '#ac2bac';
-const colorTwitch= '#a970ff';
-const colorGithub= '#333333';
+const colorFacebook = "#3b5998";
+const colorTwitter = "#55acee";
+const colorInstagram = "#ac2bac";
+const colorTwitch = "#a970ff";
+const colorGithub = "#333333";
 
 function Footer() {
   return (
@@ -21,11 +21,45 @@ function Footer() {
         <div className="footer-container">
           <div className="footer-content">
             <div className="icons-social-media">
-              <FaFacebook color={colorFacebook} size={28}/>
-              <FaTwitter color={colorTwitter} size={28}/>
-              <FaTwitch color={colorTwitch} size={28}/>
-              <FaGithub color={colorGithub} style={{filter: 'invert(1)'}} size={28}/>
-              <FaInstagram color={colorInstagram} size={28}/>
+              <a
+                href="https://www.facebook.com/joaquinbravo2000"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <FaFacebook color={colorFacebook} size={28} />
+              </a>
+              <a
+                href="https://twitter.com/Joaacooo___"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <FaTwitter color={colorTwitter} size={28} />
+              </a>
+              <a
+                href="https://www.twitch.tv/1tsjoaco"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <FaTwitch color={colorTwitch} size={28} />
+              </a>
+              <a
+                href="https://github.com/jcruzbravo"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <FaGithub
+                  color={colorGithub}
+                  style={{ filter: "invert(1)" }}
+                  size={28}
+                />
+              </a>
+              <a
+                href="https://www.instagram.com/1tsjoaco/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <FaInstagram color={colorInstagram} size={28} />
+              </a>
             </div>
 
             <div className="description">
