dba04cd235414bf288bcd985ad2feb5b7a67fa1b
jcruzbravo <L18390012@chetumal.tecnm.mx>
2023-03-03 21:00:20 -0500

    Add component lazy loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,31 @@
+import { lazy, Suspense } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Footer from "./components/Footer";
-import Header from "./components/Header";
-import Home from "./container/Home";
+const Home = lazy(() => import("./container/Home"));
 
 function App() {
   return (
     <BrowserRouter>
-      <Header />
-      <div className="App">
-        <main>
-          <div className="main-content">
-            <Routes>
-              <Route
-                path="/"
-                element={<Home/>}
-              />
-              <Route path="/shop" element={<h1>Hola, soy la tienda</h1>} />
-              <Route path="/map" element={<h1>Hola, soy el mapa</h1>} />
-              <Route path="/challenges" element={<h1>Hola, soy el mapa</h1>} />
-            </Routes>
-          </div>
-        </main>
-      </div>
-      <Footer />
+      <Suspense fallback={<div>Loading...</div>}>
+        <Header />
+        <div className="App">
+          <main>
+            <div className="main-content">
+              <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/shop" element={<h1>Hola, soy la tienda</h1>} />
+                <Route path="/map" element={<h1>Hola, soy el mapa</h1>} />
+                <Route
+                  path="/challenges"
+                  element={<h1>Hola, soy el mapa</h1>}
+                />
+              </Routes>
+            </div>
+          </main>
+        </div>
+        <Footer />
+      </Suspense>
     </BrowserRouter>
   );
 }
