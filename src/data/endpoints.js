c9718514bb53ba72f703ce7db2911a5a964a1ca1
jcruzbravo <L18390012@chetumal.tecnm.mx>
2023-03-05 09:42:19 -0500

    Add file for endpoints

---
+++ src/data/endpoints.js
@@ -0,0 +1,7 @@
+export const endpoints = {
+  DAILY_SHOP: "https://fortniteapi.io/v2/shop?lang=en",
+  NEWS: "https://fortniteapi.io/v1/news?lang=en&type=br",
+  FULL_SHOP: "https://fortniteapi.io/v2/shop?lang=en"
+}
+
+export const API_KEY = "c347d320-0c236446-aaff81e9-9ecffd56";
