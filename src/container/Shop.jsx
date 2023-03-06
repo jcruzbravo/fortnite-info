87baf2b3f6d7638479886f66cd40951100b25ba5
jcruzbravo <L18390012@chetumal.tecnm.mx>
2023-03-06 15:57:04 -0500

    Add Paginator

--- src/container/Shop.jsx
+++ src/container/Shop.jsx
@@ -1,18 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import ItemShop from "../components/ItemShop";
+import Paginator from "../components/Paginator";
 import useGetFullShop from "../hooks/useGetFullShop";
 import "../styles/Shop.scss";
 
 const Shop = () => {
   const fullShop = useGetFullShop();
+  const [page, setPage] = useState(1);
+  const [byPage, setbyPage] = useState(4);
+
+  const maximum = Math.ceil(fullShop.length / byPage);
 
   return (
     <div className="Shop">
       <div className="shop-container">
-        {fullShop.map((item) => (
-          <ItemShop key={item.mainId} item={item} />
-        ))}
+        {fullShop
+          .slice((page - 1) * byPage, (page - 1) * byPage + byPage)
+          .map((item) => (
+            <ItemShop key={item.mainId} item={item} />
+          ))}
       </div>
+      <Paginator page={page} setPage={setPage} maximum={maximum} />
     </div>
   );
 };
