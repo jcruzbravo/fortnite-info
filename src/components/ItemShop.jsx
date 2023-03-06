29cc459bb7f6930d6a0a6f3f345b4f408a502ef1
jcruzbravo <L18390012@chetumal.tecnm.mx>
2023-03-06 14:21:25 -0500

    Add the Skeleton Loader to every item in the shop

--- src/components/ItemShop.jsx
+++ src/components/ItemShop.jsx
@@ -1,19 +1,60 @@
 import React from "react";
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
+import useLoadingTimeout from "../hooks/useLoadingTimeout";
 import "../styles/ItemShop.scss";
 
 const ItemShop = ({ item }) => {
+  const isLoading = useLoadingTimeout(4000);
 
-  return (
-    <div className="ItemShop">
-      <h3>{item.displayName}</h3>
-      <div className="item-shop-image">
-        <img
-          src={item.displayAssets[0].full_background}
-          alt={item.displayName}
-        />
+  if (isLoading) {
+    return (
+      <div className="ItemShop">
+        <div className="item-shop-content">
+          <h3>
+            <Skeleton
+              count={1}
+              width={100}
+              baseColor={"#222529"}
+              highlightColor={"#42464c"}
+            />
+          </h3>
+          <p>
+            <Skeleton
+              count={1}
+              width={100}
+              baseColor={"#222529"}
+              highlightColor={"#42464c"}
+            />
+          </p>
+        </div>
+        <div className="item-shop-image">
+          <Skeleton
+            count={1}
+            width={400}
+            height={400}
+            baseColor={"#222529"}
+            highlightColor={"#42464c"}
+          />
+        </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    return (
+      <div className="ItemShop">
+        <div className="item-shop-content">
+          <h3>{item.displayName}</h3>
+          <p>{item.rarity.name}</p>
+        </div>
+        <div className="item-shop-image">
+          <img
+            src={item.displayAssets[0].full_background}
+            alt={item.displayName}
+          />
+        </div>
+      </div>
+    );
+  }
 };
 
 export default ItemShop;
