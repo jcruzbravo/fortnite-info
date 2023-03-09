49bdc382141effe1b2275bacb6b646cb635f983f
jcruzbravo <L18390012@chetumal.tecnm.mx>
2023-03-08 21:01:42 -0500

    Add Spinner component

---
+++ src/components/Spinner.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { FadeLoader } from "react-spinners";
+import "../styles/Spinner.scss";
+
+const Spinner = () => {
+  return (
+    <div className='Spinner'>
+      <FadeLoader color="#36d7b7"/>
+    </div>
+  )
+}
+
+export default Spinner;
