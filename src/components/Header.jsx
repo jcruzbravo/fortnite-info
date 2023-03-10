cd293cd4b5775f04cef4a7582fdb45e98904cccf
jcruzbravo <L18390012@chetumal.tecnm.mx>
2023-03-09 19:58:57 -0500

    Remove NavLink map

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,21 +1,18 @@
 import React from "react";
 import { NavLink, Link } from "react-router-dom";
 import logo from "../assets/logo.png";
-import '../styles/Header.scss';
+import "../styles/Header.scss";
 
 const Header = () => {
   return (
     <header>
       <Link to="/">
-        <img src={logo} alt="home"/>
+        <img src={logo} alt="home" />
       </Link>
       <nav>
         <ul>
           <li>
             <NavLink to="/shop">Shop</NavLink>
-          </li>
-          <li>
-            <NavLink to="/map">Map</NavLink>
           </li>
           <li>
             <NavLink to="/challenges">Challenges</NavLink>
