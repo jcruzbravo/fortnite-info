3fbb366b63b226e8b27a7b081f75eb0bb2f6f423
jcruzbravo <L18390012@chetumal.tecnm.mx>
2023-03-11 16:30:02 -0500

    Add paginator

--- src/container/Challenges.jsx
+++ src/container/Challenges.jsx
@@ -1,20 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import ItemChallenge from "../components/ItemChallenge";
+import Paginator from "../components/Paginator";
 import useGetChallenges from "../hooks/useGetChallenges";
 import "../styles/Challenges.scss";
 
 const Challenges = () => {
   const challenges = useGetChallenges();
-  const challengesWithImageAndBundleName = challenges.filter(
-    (challenge) => challenge.name != null && challenge.image != null
-  );
+  const challengesWithImageAndBundleName = challenges.filter((challenge) => challenge.name != null && challenge.image != null);
+
+  const [page, setPage] = useState(1);
+  const [byPage, setbyPage] = useState(1);
+  const maximum = Math.ceil(challengesWithImageAndBundleName.length / byPage);
+
   return (
     <section>
       <div className="Challenges">
-        {challengesWithImageAndBundleName.map((challenge) => (
+        {challengesWithImageAndBundleName.slice((page - 1) * byPage, (page - 1) * byPage + byPage).map((challenge) => (
           <ItemChallenge key={challenge.tag} challenge={challenge} />
         ))}
       </div>
+      <Paginator page={page} setPage={setPage} maximum={maximum} />
     </section>
   );
 };
