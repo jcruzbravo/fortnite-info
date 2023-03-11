acc8dfec1bde0386da2572fe7bd763611eb168b8
jcruzbravo <L18390012@chetumal.tecnm.mx>
2023-03-11 16:28:52 -0500

    Add filter to show active missions and fix styles

--- src/components/ItemChallenge.jsx
+++ src/components/ItemChallenge.jsx
@@ -2,15 +2,30 @@ import React from "react";
 import "../styles/ItemChallenge.scss";
 
 const ItemChallenge = ({ challenge }) => {
+  const bundles = challenge.bundles;
+  const bundlesWithQuestActives = bundles.filter(bundle => bundle.quests.some(quest => quest.enabled === true));
+  console.log(bundlesWithQuestActives);
+
   return (
     <div className="ItemChallenge">
       <div className="item-challenge-container">
-        <div className="info-container">
-          <p>{challenge.name}</p>
-          <button>See challenges</button>
+        <div className="info-header">
+          <h3>{challenge.name}</h3>
+          <div className="img-container">
+            <img src={challenge.image} alt="" />
+          </div>
         </div>
-        <div className="img-container">
-          <img src={challenge.image} alt="" />
+        <div className="info-content">
+          {bundlesWithQuestActives.map((bundle) => (
+            <div className="info-quests-content">
+              <h4>{bundle.name}</h4>
+              {bundle.quests.map((quest) => (
+                <div>
+                  <h5>{quest.name}</h5>
+                </div>
+              ))}
+            </div>
+          ))}
         </div>
       </div>
     </div>
