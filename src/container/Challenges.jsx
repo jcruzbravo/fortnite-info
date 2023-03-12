15fd8ece47b2f8782ac76c96eb6987cd1cb86861
jcruzbravo <L18390012@chetumal.tecnm.mx>
2023-03-11 20:55:51 -0500

    Add lazy loading and suspense

--- src/container/Challenges.jsx
+++ src/container/Challenges.jsx
@@ -1,26 +1,33 @@
-import React, { useState } from "react";
-import ItemChallenge from "../components/ItemChallenge";
-import Paginator from "../components/Paginator";
+import React, { Suspense, useState, lazy } from "react";
+const ItemChallenge = lazy(() => import("../components/ItemChallenge"));
+const Paginator = lazy(() => import("../components/Paginator"));
 import useGetChallenges from "../hooks/useGetChallenges";
 import "../styles/Challenges.scss";
+import Spinner from "../components/Spinner";
 
 const Challenges = () => {
   const challenges = useGetChallenges();
-  const challengesWithImageAndBundleName = challenges.filter((challenge) => challenge.name != null && challenge.image != null);
+  const challengesWithImageAndBundleName = challenges.filter(
+    (challenge) => challenge.name != null && challenge.image != null
+  );
 
   const [page, setPage] = useState(1);
   const [byPage, setbyPage] = useState(1);
   const maximum = Math.ceil(challengesWithImageAndBundleName.length / byPage);
 
   return (
-    <section>
-      <div className="Challenges">
-        {challengesWithImageAndBundleName.slice((page - 1) * byPage, (page - 1) * byPage + byPage).map((challenge) => (
-          <ItemChallenge key={challenge.tag} challenge={challenge} />
-        ))}
-      </div>
-      <Paginator page={page} setPage={setPage} maximum={maximum} />
-    </section>
+    <Suspense fallback={<Spinner/>}>
+      <section>
+        <div className="Challenges">
+          {challengesWithImageAndBundleName
+            .slice((page - 1) * byPage, (page - 1) * byPage + byPage)
+            .map((challenge) => (
+              <ItemChallenge key={challenge.tag} challenge={challenge} />
+            ))}
+        </div>
+        <Paginator page={page} setPage={setPage} maximum={maximum} />
+      </section>
+    </Suspense>
   );
 };
 
