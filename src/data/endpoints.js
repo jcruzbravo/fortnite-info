4dc9d81b66451039b83ceaeeff8794fa0401a444
jcruzbravo <L18390012@chetumal.tecnm.mx>
2023-03-11 20:56:02 -0500

    Change language

--- src/data/endpoints.js
+++ src/data/endpoints.js
@@ -2,7 +2,7 @@ export const endpoints = {
   DAILY_SHOP: "https://fortniteapi.io/v2/shop?lang=en",
   NEWS: "https://fortniteapi.io/v1/news?lang=en&type=br",
   FULL_SHOP: "https://fortniteapi.io/v2/shop?lang=en",
-  CHALLENGES: "https://fortniteapi.io/v3/challenges?season=current&lang=es"
+  CHALLENGES: "https://fortniteapi.io/v3/challenges?season=current&lang=en"
 }
 
 export const API_KEY = "c347d320-0c236446-aaff81e9-9ecffd56";
