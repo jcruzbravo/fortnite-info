ca9a841723cb4dc0e455b245d5e73c06eeebd443
jcruzbravo <L18390012@chetumal.tecnm.mx>
2023-03-13 15:56:35 -0500

    Add stats player endpoint and new API key

--- src/data/endpoints.js
+++ src/data/endpoints.js
@@ -2,7 +2,10 @@ export const endpoints = {
   DAILY_SHOP: "https://fortniteapi.io/v2/shop?lang=en",
   NEWS: "https://fortniteapi.io/v1/news?lang=en&type=br",
   FULL_SHOP: "https://fortniteapi.io/v2/shop?lang=en",
-  CHALLENGES: "https://fortniteapi.io/v3/challenges?season=current&lang=en"
-}
+  CHALLENGES: "https://fortniteapi.io/v3/challenges?season=current&lang=en",
+  STATS_PLAYER: "https://fortnite-api.com/v2/stats/br/v2",
+};
 
-export const API_KEY = "c347d320-0c236446-aaff81e9-9ecffd56";
+export const API_KEY_FORTNITE_IO = "c347d320-0c236446-aaff81e9-9ecffd56";
+
+export const API_KEY_FORTNITE = "a399d3b6-f709-431a-b8cb-b88843271ab2";
