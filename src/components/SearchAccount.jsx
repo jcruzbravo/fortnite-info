284c1a17b3df5d2541aacf4430cd682306d995ac
jcruzbravo <L18390012@chetumal.tecnm.mx>
2023-03-15 14:10:37 -0500

    Add Search Account component

---
+++ src/components/SearchAccount.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import useGetStatsPlayer from "../hooks/useGetStatsPlayer";
+import "../styles/SearchAccount.scss";
+import Spinner from "./Spinner";
+import Profile from "../container/Profile";
+
+const SearchAccount = () => {
+  const {
+    statsPlayer,
+    message,
+    usernameInput,
+    isLoading,
+    handleInput,
+    handleSearch,
+  } = useGetStatsPlayer();
+
+  return (
+    <section>
+      <div className="SearchAccount">
+        <div className="search-account-container">
+          <div className="account-player-stats-form">
+            <input
+              autoComplete="off"
+              name="username"
+              placeholder="Write your name account"
+              value={usernameInput}
+              onChange={handleInput}
+            />
+            <button onClick={handleSearch}>Search</button>
+          </div>
+        </div>
+        {isLoading ? (
+          <Spinner />
+        ) : Object.keys(statsPlayer).length > 0 ? (
+          <Profile key={statsPlayer.account.id} statsPlayer={statsPlayer} />
+        ) : (
+          <div>
+            <h3>{message}</h3>
+          </div>
+        )}
+      </div>
+    </section>
+  );
+};
+
+export default SearchAccount;
