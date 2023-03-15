4968c10d01b168a9143609879c88ac70f4263668
jcruzbravo <L18390012@chetumal.tecnm.mx>
2023-03-15 14:12:19 -0500

    Add SearchAccount component to Account container

---
+++ src/container/Account.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import SearchAccount from '../components/SearchAccount'
+
+const Account = () => {
+  return (
+    <section>
+      <div className='Account'>
+        <SearchAccount/>
+      </div>
+    </section>
+  )
+}
+
+export default Account
