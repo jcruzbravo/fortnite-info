47a2e9ef8bcdefadebeb6ce553747b7f2ff7729f
jcruzbravo <L18390012@chetumal.tecnm.mx>
2023-05-06 14:17:59 -0500

    Test 2

--- src/data/endpoints.js
+++ src/data/endpoints.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = process.env.VITE_API_URL || 'http://localhost:3000';
+const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:3000';
 
 export const endpoints = {
   DAILY_SHOP: `${API_BASE_URL}/api/daily_shop`,
