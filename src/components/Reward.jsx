ae6f43eac74c77cbb6daca6eb4c38b84e4b39473
jcruzbravo <L18390012@chetumal.tecnm.mx>
2023-06-17 18:37:23 -0500

    ADD: icon to see the reward

--- src/components/Reward.jsx
+++ src/components/Reward.jsx
@@ -1,9 +1,22 @@
-import React from "react";
+import React, { useContext } from "react";
+import { FortniteInformerContext } from "../context";
+import { FaEye } from "react-icons/fa";
 import "../styles/Reward.scss";
 
 const Reward = ({ reward }) => {
+
+  const context = useContext(FortniteInformerContext);
+
+  const showRewardDetail = (object) => {
+    context.openRewardDetail();
+    context.setRewardToShow(object);
+  }
+
   return (
     <div className="Reward">
+      <span className="view" onClick={() => showRewardDetail(reward)}>
+        <FaEye />
+      </span>
       <h2>{reward.item.name}</h2>
       <p>{reward.item.rarity.name}</p>
       <figure style={{ width: "150px" }}>
